dd97b09686b1bfb45ee809874c45c265509b900e
Rob Bast <rob.bast@gmail.com>
2016-11-07 14:47:05 +0100

    make entrypoint scripts "smarter"
    attempt to discern between whether or not the user wants to run a
    composer command or not, and behave accordingly

--- 1.1/docker-entrypoint.sh
+++ 1.1/docker-entrypoint.sh
@@ -3,15 +3,64 @@
 DOCKER_UID=${DOCKER_UID:-1}
 DOCKER_USER=${DOCKER_USER:-root}
 
-set -- composer "$@"
+isCommand() {
+  for cmd in \
+    "about" \
+    "archive" \
+    "browse" \
+    "clear-cache" \
+    "clearcache" \
+    "config" \
+    "create-project" \
+    "depends" \
+    "diagnose" \
+    "dump-autoload" \
+    "dumpautoload" \
+    "exec" \
+    "global" \
+    "help" \
+    "home" \
+    "info" \
+    "init" \
+    "install" \
+    "licenses" \
+    "list" \
+    "outdated" \
+    "prohibits" \
+    "remove" \
+    "require" \
+    "run-script" \
+    "search" \
+    "self-update" \
+    "selfupdate" \
+    "show" \
+    "status" \
+    "suggests" \
+    "update" \
+    "validate" \
+    "why" \
+    "why-not"
+  do
+    if [ -z "${cmd#"$1"}" ]; then
+      return 0
+    fi
+  done
 
-# run under uid/user specified if not equal to defaults
-if [ "root" != "$DOCKER_USER" ] && [ "1" != "$DOCKER_UID" ];
-then
-    adduser -H -D -u $DOCKER_UID $DOCKER_USER
-    chown -R $DOCKER_USER "$COMPOSER_HOME"
+  return 1
+}
 
-    set -- su-exec $DOCKER_USER "$@"
+# see if the first argument passed in looks like a flag
+if [ "$(printf %c "$1")" == '-' ]; then
+  set -- /sbin/tini -- composer "$@"
+# see if the first argument passed in matches a known command
+elif isCommand "$1"; then
+  set -- /sbin/tini -- composer "$@"
 fi
 
-exec /sbin/tini -- "$@"
+# run under uid/user specified if not equal to defaults
+if [ "root" != "$DOCKER_USER" ] && [ "1" != "$DOCKER_UID" ]; then
+  adduser -H -D -u "$DOCKER_UID" "$DOCKER_USER"
+  set -- su-exec "$DOCKER_USER" "$@"
+fi
+
+exec "$@"
