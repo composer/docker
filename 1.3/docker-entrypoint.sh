f78495e8f03f51b69dfb3709629523caba45a137
Mario Inga <mario@yarkan.pe>
2017-02-06 09:36:27 -0500

    fix(1.3) replacing gosu by su-exec

--- 1.3/docker-entrypoint.sh
+++ 1.3/docker-entrypoint.sh
@@ -3,4 +3,4 @@
 sed -ie "s/$TMP_UID/$DEV_UID/g" /etc/passwd
 sed -ie "s/$TMP_UID/$DEV_GID/g" /etc/group
 
-exec /sbin/tini -- gosu $DEV_UID:$DEV_GID composer "$@"
+exec /sbin/tini -- su-exec $TMP_USER composer "$@"
