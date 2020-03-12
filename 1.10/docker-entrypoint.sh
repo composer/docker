6deedc6a8c89b17c9a6ca61a28c8b206513c76f8
Rob Bast <rob.bast@gmail.com>
2020-03-12 13:44:46 +0100

    retain "BC" for CI providers, fixes #107

--- 1.10/docker-entrypoint.sh
+++ 1.10/docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 isCommand() {
+  # Retain backwards compatibility with common CI providers,
+  # see: https://github.com/composer/docker/issues/107
+  if [ "$1" == "sh" ]; then
+    return 1
+  fi
+
   composer help $1 > /dev/null 2>&1
 }
 
