19184c1ec6538dbea9320280551d925af5f02b41
Rob Bast <rob.bast@gmail.com>
2020-05-14 10:47:08 +0200

    apply shellcheck suggestions against stackbrew script

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-self="$(basename "$BASH_SOURCE")"
+self="$(basename "${BASH_SOURCE[0]}")"
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" || exit
 
 # get the most recent commit which modified any of "$@"
 fileCommit() {
@@ -13,21 +13,21 @@ fileCommit() {
 dirCommit() {
 	local dir="$1"; shift
 	(
-		cd "$dir"
+		cd "$dir" || exit
 		fileCommit Dockerfile \
-			$(git show HEAD:./Dockerfile | awk '
+			"$(git show HEAD:./Dockerfile | awk '
 				toupper($1) == "COPY" {
 					for (i = 2; i < NF; i++) {
 						print $i
 					}
 				}
-			')
+			')"
 	)
 }
 
 # determine actual composer version based on COMPOSER_VERSION value
 extractVersion() {
-	git show "$1":"$2/Dockerfile" | awk '$1 == "ENV" && $2 == "COMPOSER_VERSION" { print $3; exit }'
+	git show "$1:$2/Dockerfile" | awk '$1 == "ENV" && $2 == "COMPOSER_VERSION" { print $3; exit }'
 }
 
 getArches() {
@@ -35,7 +35,7 @@ getArches() {
 	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
 
 	eval "declare -g -A parentRepoToArches=( $(
-		find -name 'Dockerfile' -exec awk '
+		find . -name 'Dockerfile' -exec awk '
 				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|microsoft\/[^:]+)(:|$)/ {
 					print "'"$officialImagesUrl"'" $2
 				}
@@ -44,6 +44,8 @@ getArches() {
 			| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
 	) )"
 }
+
+declare parentRepoToArches
 getArches 'composer'
 
 # prints "$2$1$3$1...$N"
@@ -55,9 +57,7 @@ join() {
 
 directories=( */ )
 directories=( "${directories[@]%/}" )
-
-# sort directories descending
-IFS=$'\n'; directories=( $(echo "${directories[*]}" | sort -rV) ); unset IFS
+mapfile -n directories < <(echo "${directories[*]}" | sort -rV)
 
 declare -A aliases=(
 	[1.10]='1 latest'
@@ -75,14 +75,14 @@ EOH
 for directory in "${directories[@]}"; do
 	commit="$(dirCommit "$directory")"
 	version="$(extractVersion "$commit" "$directory")"
-	tags=($version $directory ${aliases[$directory]:-})
+	tags=("$version" "$directory" "${aliases[$directory]:-}")
 	variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$directory/Dockerfile")"
 	variantArches="${parentRepoToArches[$variantParent]}"
 
 	cat <<-EOE
 
 		Tags: $(join ', ' "${tags[@]}")
-		Architectures: $(join ', ' $variantArches)
+		Architectures: $(join ', ' "$variantArches")
 		GitCommit: $commit
 		Directory: $directory
 	EOE
